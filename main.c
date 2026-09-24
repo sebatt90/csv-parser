8c1a763230b226ceccdc5ffa86c3322555c07ecd
Sebastiano Babich <babich.sebastiano@proton.me>
2026-09-24 16:39:25 +0200

    Multithreaded
    made the code multithreaded

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <pthread.h>
+
+#define THREAD_SLICE 10485760
 
 char *read_file(char *path){
   FILE *fp = fopen(path, "r");
@@ -34,7 +38,7 @@ typedef struct array_t {
 } CSVArray;
 
 
-#define CSVARRAY_SIZE 16
+#define CSVARRAY_SIZE 8192
 
 CSVArray *csvarray_init(){
   CSVArray *n = (CSVArray *) calloc(1,sizeof(CSVArray));
@@ -51,35 +55,6 @@ static void csvarray_insert(CSVArray *arr, char *data){
   }
   arr->data[arr->len] = data;
   arr->len++;
-}
-
-// list implementation
-typedef struct list_t {
-  char *entry;
-  struct list_t *next;
-} MyList;
-
-
-MyList *list_insert_prev(MyList *p, char *ptr) {
-  MyList *n = (MyList *)malloc(sizeof(MyList));
-  n->entry = ptr;
-  n->next = NULL;
-  
-  if(p != NULL){
-    p->next = n;
-  }
-
-  return n;
-} 
-
-MyList *list_destroy(MyList *h){
-  MyList *c = h;
-  MyList *p = NULL;
-  while (c != NULL){
-    p = c;
-    c = c->next;
-    free(p);
-  }
 }
 
 CSVArray *csv_list(char *csv) {
@@ -105,15 +80,63 @@ CSVArray *csv_list(char *csv) {
   return arr;
 }
 
+typedef struct csvslice_t {
+  size_t start;
+  size_t end;
+  CSVArray *arr;
+} CSVSlice;
+
+static char *csv;
+
+void *process_csv(void *args) {
+  CSVSlice *slice = (CSVSlice *) args;
+
+  slice->arr = csvarray_init();
+
+  bool flag = (slice->start == 0) ? true : false;
+
+  for (size_t i=slice->start; i<slice->end;i++) {
+    if(csv[i] == ',' || csv[i] == '\n' || csv[i] == '\r') {
+      csv[i] = 0;
+      flag = true;
+      continue;
+    }
+    
+    if(flag == true) {
+      csvarray_insert(slice->arr, csv+i);
+      flag = false;
+    }
+  }
+  
+}
+
 int main(int argc, char **argv) {
   if(argc < 2) { fprintf(stderr, "[ERROR] please provide file path\n");  exit(-1);}
 
-  char *csv = read_file(argv[1]);
+  csv = read_file(argv[1]);
+  size_t csvlen = strlen(csv);
 
-  CSVArray *csvarr = csv_list(csv);
+  size_t N_THREADS = (csvlen + THREAD_SLICE - 1) / THREAD_SLICE;
+
+  printf("len: %d; slice: %d; threads: %d\n",csvlen,THREAD_SLICE,N_THREADS);
   
-  free(csv);
-  free(csvarr->data);
-  free(csvarr);
+  pthread_t threads[N_THREADS];
+  CSVSlice *slices[N_THREADS];
+  
+  for(size_t i=0; i<N_THREADS; i++){
+    slices[i] = (CSVSlice *) malloc(sizeof(CSVSlice));
+    slices[i]->start = i*THREAD_SLICE;
+    slices[i]->end = (i == N_THREADS - 1) ? strlen(csv) - 1 : (i+1)*THREAD_SLICE - 1;
+    pthread_create(&threads[i], NULL, process_csv, slices[i]);
+  }
+
+  
+  for (int i = 0; i < N_THREADS; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  // merge the array
+
+  
   return 0;
 }
